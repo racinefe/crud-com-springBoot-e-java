747a92e4a06b73c7063f08f0364573d8766f1b04
@racinefell <102471647+racinefe@users.noreply.github.com>
2025-01-05 10:32:15 -0300

    Criando Deletar Usuário por id

--- crud-com-springBoot-e-java/src/main/java/br/com/crud/controller/UsuarioController.java
+++ crud-com-springBoot-e-java/src/main/java/br/com/crud/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,10 @@ public class UsuarioController {
 		}
 	}
 	//DeletarUsuário
-	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletarUsuario(@PathVariable Long id){
+		usuarioService.deletarUsuario(id);
+		return ResponseEntity.noContent().build();
+				
+	}
 }
